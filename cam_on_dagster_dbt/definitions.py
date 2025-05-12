0ca48b644cc3df30ab2d1a5eed96ff8e017268ee
Cam <cam.martin0505@gmail.com>
2025-05-12 04:26:32 +0000

    Dependent Pipelines

--- cam_on_dagster_dbt/definitions.py
+++ cam_on_dagster_dbt/definitions.py
@@ -3,6 +3,7 @@ from dagster import Definitions, define_asset_job
 from cam_on_dagster_dbt.assets.gsheets_assets import gsheet_finance_data
 from cam_on_dagster_dbt.assets.gsheets_customdbt import run_dbt_command
 from cam_on_dagster_dbt.assets.beverage_dim_assets import beverage_dim_data
+from cam_on_dagster_dbt.assets.beverage_dim_data_assets import dimension_data
 # Import only the necessary job
 from cam_on_dagster_dbt.jobs.gsheets_job import gsheets_financial_with_dbt_job
 from cam_on_dagster_dbt.jobs.beverage_dim_job import beverage_dim_job
@@ -10,7 +11,8 @@ from cam_on_dagster_dbt.sensors import camon_sensor
 from cam_on_dagster_dbt.schedules import schedules
 
 # Define the assets
-all_assets = [gsheet_finance_data, run_dbt_command, beverage_dim_data]
+all_assets = [gsheet_finance_data, run_dbt_command,
+              beverage_dim_data, dimension_data]
 
 # Register the job, sensor, and schedule in the Definitions
 defs = Definitions(
--- cam_on_dagster_dbt/jobs/beverage_dim_job.py
+++ cam_on_dagster_dbt/jobs/beverage_dim_job.py
@@ -1,9 +1,13 @@
 from dagster import job
 from cam_on_dagster_dbt.assets.beverage_dim_assets import beverage_dim_data
+from cam_on_dagster_dbt.assets.beverage_dim_data_assets import dimension_data
 # from cam_on_dagster_dbt.assets.gsheets_customdbt import run_dbt_command
 
 
 @job
 def beverage_dim_job():
     # First, run the gsheet_finance_data asset
-    beverage_dim_data()
+    beverage_dim_values = beverage_dim_data()
+
+    # Then, run the dim data asset
+    dimension_data(beverage_dim_values)
