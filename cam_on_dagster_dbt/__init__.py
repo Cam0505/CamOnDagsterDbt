f97f78813d760946ada7533cd0fe8ec9ec7d3793
Cam <cam.martin0505@gmail.com>
2025-05-13 04:24:34 +0000

    Cleaning up GSheets Assets and Jobs, adding custom logging tto Assets to prevent re-runs in less than 30minutes

--- cam_on_dagster_dbt/__init__.py
+++ cam_on_dagster_dbt/__init__.py
@@ -4,6 +4,7 @@
 
 from .assets.dbt_assets import camon_dbt_assets
 from .jobs.gsheets_job import gsheets_financial_with_dbt_job
+from .assets.Gsheets import gsheet_finance_data, gsheet_dbt_command
 from .jobs.jobs import run_dbt_assets
 from .schedules import schedules
 from .sensors import camon_sensor
--- cam_on_dagster_dbt/jobs/jobs.py
+++ cam_on_dagster_dbt/jobs/jobs.py
@@ -1,7 +1,6 @@
 from dagster import job
 from cam_on_dagster_dbt.assets.dbt_assets import camon_dbt_assets
-from cam_on_dagster_dbt.assets.gsheets_assets import gsheet_finance_data
-from cam_on_dagster_dbt.assets.gsheets_customdbt import run_dbt_command
+from cam_on_dagster_dbt.assets.Gsheets import gsheet_finance_data, gsheet_dbt_command
 from cam_on_dagster_dbt.jobs.gsheets_job import gsheets_financial_with_dbt_job
 
 
@@ -9,8 +8,3 @@ from cam_on_dagster_dbt.jobs.gsheets_job import gsheets_financial_with_dbt_job
 @job
 def run_dbt_assets():
     camon_dbt_assets()
-
-# Define job for gsheets and custom dbt run
-
-
-gsheets_financial_with_dbt_job
