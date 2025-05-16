c99b86c52299ef6d315424e961c5fd1d45b79ffe
Cam <cam.martin0505@gmail.com>
2025-05-16 09:50:37 +0000

    Lost my duckdb file

--- cam_on_dagster_dbt/definitions.py
+++ cam_on_dagster_dbt/definitions.py
@@ -2,8 +2,8 @@
 from dagster import Definitions, define_asset_job
 
 # Import Rick and Morty assets and jobs (active)
-from cam_on_dagster_dbt.assets import rick_and_morty_asset, dbt_rick_and_morty_data
-from cam_on_dagster_dbt.jobs import RickandMorty_job
+# from cam_on_dagster_dbt.assets import rick_and_morty_asset, dbt_rick_and_morty_data
+# from cam_on_dagster_dbt.jobs import RickandMorty_job
 
 # Uncomment and import other assets/jobs as needed:
 
@@ -17,7 +17,7 @@ from cam_on_dagster_dbt.jobs import RickandMorty_job
 # from cam_on_dagster_dbt.assets import meals_dim_data, meals_dimension_data, meals_fact_data, dbt_meals_data
 
 # OpenLibrary Assets
-# from cam_on_dagster_dbt.assets import openlibrary_books_asset, dbt_openlibrary_data
+from cam_on_dagster_dbt.assets import openlibrary_books_asset, dbt_openlibrary_data
 
 # GeoAPI Assets
 # from cam_on_dagster_dbt.assets import get_geo_data, dbt_geo_data
@@ -27,29 +27,27 @@ from cam_on_dagster_dbt.jobs import RickandMorty_job
 # from cam_on_dagster_dbt.jobs import beverage_dim_job
 # from cam_on_dagster_dbt.jobs import meals_dim_job
 # from cam_on_dagster_dbt.jobs import geo_data_job
-# from cam_on_dagster_dbt.jobs import openlibrary_job
+from cam_on_dagster_dbt.jobs import openlibrary_job
 
 from cam_on_dagster_dbt.sensors import camon_sensor
 from cam_on_dagster_dbt.schedules import schedules
 
 # Define the assets
-all_assets = [rick_and_morty_asset, dbt_rick_and_morty_data]
+all_assets = [openlibrary_books_asset, dbt_openlibrary_data]
 
 # Register the job, sensor, and schedule in the Definitions
 defs = Definitions(
     assets=all_assets,
     # Register only the gsheets job
-    jobs=[RickandMorty_job],
+    jobs=[openlibrary_job],
     schedules=[schedules]  # ,
     # sensors=[camon_sensor]
 )
 
 # Execute the job immediately
 if __name__ == "__main__":
-    # Run the gsheets_financial_with_dbt_job once immediately
-    # result = gsheets_financial_with_dbt_job.execute_in_process()
     try:
-        result = RickandMorty_job.execute_in_process()
-        print("RickandMorty_job Job finished:", result.success)
+        result = openlibrary_job.execute_in_process()
+        print("openlibrary_job Job finished:", result.success)
     except Exception as e:
         print(f"Error executing job: {e}")
