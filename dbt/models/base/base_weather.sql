b0610bffcbae52eb6c02bd29dc9d52af9a9332e0
Cam <cam.martin0505@gmail.com>
2025-05-18 02:05:06 +0000

    Fixing errors in new weather tables

--- dbt/models/base/base_weather.sql
+++ dbt/models/base/base_weather.sql
@@ -1,5 +1,5 @@
 
 
 SELECT (date)::date as weather_date, city, temperature_max, temperature_min, temperature_mean, precipitation_sum, 
-windspeed_max, windgusts_max, sunshine_duration, location__lat, location__lng, timestamp, "_dlt_load_id", "_dlt_id"
+windspeed_max, windgusts_max, sunshine_duration, location__lat as latitude, location__lng as longitude
 FROM {{ source("weather", "daily_weather") }}
