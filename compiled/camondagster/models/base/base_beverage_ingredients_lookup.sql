88d7206875b590c78120d2c3f60beda4110fe028
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 00:58:06 +0000

    deploy: 66e7ca8d7c57364fa30d3d16fb23955d1db3bdb8

--- compiled/camondagster/models/base/base_beverage_ingredients_lookup.sql
+++ compiled/camondagster/models/base/base_beverage_ingredients_lookup.sql
@@ -1,7 +1,5 @@
-Select str_ingredient1 as Ingredient,
+Select it.source_ingredient as Ingredient,
 it.id_drink as beverage_id,
 it.str_drink as Beverage_Name
-from "camondagster"."beverage_data"."ingredients" as i 
-left join "camondagster"."beverage_data"."ingredients_table"  as it
-	on i.str_ingredient1 = it.source_ingredient
+from "camondagster"."beverage_data"."ingredients_table"  as it
 where it.id_drink is not null
