269e50b92a2f92df7ccd32aec2ace613f81f064a
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 01:32:52 +0000

    deploy: a38043202a64e52b13cf74a1b7f2666cd900ac42

--- compiled/camondagster/models/base/base_beverage_ingredients_lookup.sql
+++ compiled/camondagster/models/base/base_beverage_ingredients_lookup.sql
@@ -1,3 +1,14 @@
+-- ------------------------------------------------------------------------------
+-- Model: Base_beverage_ingredients_lookup
+-- Description: Base Table for Beverage Ingredients
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-12 | Cam      | Initial creation
+-- 2025-05-12 | Cam      | Added logic to filter out null beverage IDs
+-- YYYY-MM-DD | NAME     | [Add future changes here]
+-- ------------------------------------------------------------------------------
 Select it.source_ingredient as Ingredient,
 it.id_drink as beverage_id,
 it.str_drink as Beverage_Name
