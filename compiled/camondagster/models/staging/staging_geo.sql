249a26d6daf37ec7676492a9e10fa7a10944d4ba
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 02:11:21 +0000

    deploy: 248619df89ba8251a6a40b275f284ede3ba37617

--- compiled/camondagster/models/staging/staging_geo.sql
+++ compiled/camondagster/models/staging/staging_geo.sql
@@ -1,3 +1,13 @@
+-- ------------------------------------------------------------------------------
+-- Model: Staging_geo
+-- Description: Staging model for geographic data
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-19 | Cam      | Initial creation
+-- YYYY-MM-DD | NAME     | [Add future changes here]
+-- ------------------------------------------------------------------------------
 select city_id, city, latitude, longitude, country_code, country, region, 
 md5(cast(coalesce(cast(city as TEXT), '_dbt_utils_surrogate_key_null_') || '-' || coalesce(cast(country as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as City_SK,
 md5(cast(coalesce(cast(country as TEXT), '_dbt_utils_surrogate_key_null_') as TEXT)) as Country_SK
