1c28ca59f8622c8fe15ee66029f1480afe46d897
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 01:39:09 +0000

    deploy: f224faee7c3e2b8ed4965a0e92b3193cb7e2d1a9

--- compiled/camondagster/models/base/base_geo.sql
+++ compiled/camondagster/models/base/base_geo.sql
@@ -1,3 +1,13 @@
+-- ------------------------------------------------------------------------------
+-- Model: Base_Geo
+-- Description: Base Table for multiple Dims - City, Country, Region and Continent
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-15 | Cam      | Initial creation
+-- YYYY-MM-DD | NAME     | [Add future changes here]
+-- ------------------------------------------------------------------------------
 SELECT city_id, city, latitude, longitude, country_code, country, region, continent
 From "camondagster"."geo_data"."geo_cities" 
 where country in ('New Zealand', 'United Kingdom', 'Australia', 'Canada')
