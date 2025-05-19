ebf5e369f81f59a5eebe9c23cd34b44849d71572
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 01:47:13 +0000

    deploy: 0ecf3cbd79a8735af38c70507590d277753c4dc7

--- compiled/camondagster/models/base/base_rm_location_residents.sql
+++ compiled/camondagster/models/base/base_rm_location_residents.sql
@@ -1,3 +1,15 @@
+-- ------------------------------------------------------------------------------
+-- Model: base_rm_location_residents
+-- Description: Base Table for ricky and morty residents within a location from API
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-16 | Cam      | Initial creation
+-- YYYY-MM-DD | NAME     | [Add future changes here]
+-- ------------------------------------------------------------------------------
+
+
 SELECT
   _dlt_root_id AS location_dlt_id,
   CAST(regexp_replace(value, '.*/(\d+)$', '\1') AS INTEGER) AS character_id
