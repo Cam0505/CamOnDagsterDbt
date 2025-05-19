b1bf86a71b0500b032acd0821827fca2c8eaf486
Cam0505 <Cam0505@users.noreply.github.com>
2025-05-19 01:42:27 +0000

    deploy: 212e05d4c49e8c345b38904be7c47962dd7af655

--- compiled/camondagster/models/base/base_gsheets_finance.sql
+++ compiled/camondagster/models/base/base_gsheets_finance.sql
@@ -1,7 +1,17 @@
+-- ------------------------------------------------------------------------------
+-- Model: Base Gsheets Finance
+-- Description: Base Table for Gsheets Finance
+-- ------------------------------------------------------------------------------
+-- Change Log:
+-- Date       | Author   | Description
+-- -----------|----------|-------------------------------------------------------
+-- 2025-05-14 | Cam      | Initial creation
+-- YYYY-MM-DD | NAME     | [Add future changes here]
+-- ------------------------------------------------------------------------------
 SELECT 
     Id, 
     stock, 
-    CAST(price AS DECIMAL) AS price,  -- DuckDB doesn't have a money type
+    CAST(price AS DECIMAL) AS price,
     date_time AT TIME ZONE 'UTC' AT TIME ZONE 'Australia/Melbourne' AS date_time,
     ROUND((MAX(price) OVER(PARTITION BY stock) - MIN(price) OVER(PARTITION BY stock)), 2) AS price_spread,
     ROUND((LAST(price) OVER(PARTITION BY stock ORDER BY date_time) - FIRST(price) OVER(PARTITION BY stock)), 2) AS relative_price_movement,
